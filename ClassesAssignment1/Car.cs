b340262f61efa5ae3d978755fabaf82f1b9d12dd
Bradley Wilson <bradleywilson727@gmail.com>
2022-06-10 15:33:49 -0500

    completed assignment

---
+++ ClassesAssignment1/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ClassesAssignment1
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
+}
---
+++ ClassesAssignment1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassesAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+
+            myCar.Make = "Nissan";
+
+            myCar.Model = "Rogue";
+
+            myCar.Year = 2012;
+
+            Console.WriteLine($"I drive a {myCar.Year} {myCar.Make} {myCar.Model}.");
+        }
+    }
+}
